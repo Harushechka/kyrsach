2c6c5a442d0b0061f5d3e5d87777165be43d0f78
poweljel143 <152842993+poweljel143@users.noreply.github.com>
2025-06-10 10:04:21 +0000

    накуралесила

--- my-spa-project/src/router/index.js
+++ my-spa-project/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CertificateView from '@/pages/CertificateView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   { path: '/certificate', component: CertificateView },
+  { path: '/home', component: HomeView},
   // Добавь другие маршруты по заданию
 ]
 
